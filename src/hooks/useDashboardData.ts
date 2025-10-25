51c953881599d1b416b3420e38df6585792d5319
Dwipurnomo5151 <dwipurnomo5151@gmail.com>
2025-10-25 18:49:35 +0700

    test

--- src/hooks/useDashboardData.ts
+++ src/hooks/useDashboardData.ts
@@ -1,11 +1,11 @@
-import api from "@/utils/api";
+import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 export const useDashboardData = () => {
   return useQuery({
     queryKey: ["dashboard"],
     queryFn: async () => {
-      const res = await api.get("/dashboard");
+      const res = await axios.get("https://catatin-backend-production.up.railway.app/api/dashboard");
       return res.data;
     },
   });
