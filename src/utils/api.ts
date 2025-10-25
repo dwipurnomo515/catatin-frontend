dfdd8fc4bdf470ee66233c3203702d41ce8f513d
Dwipurnomo5151 <dwipurnomo5151@gmail.com>
2025-10-25 19:32:36 +0700

    vercel.json

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,7 +7,6 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
   const token = Cookies.get('token');
-  console.log(token);
   
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
